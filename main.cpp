9485c4b8635496661e25cb8185b2fbcd04f42e6c
Preston Kemp <preston@prestonkemp.com>
2021-02-28 11:09:17 -0600

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  CS-6903-Project-1
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!!!!!\n";
+    return 0;
+}
